cb93c0cd08bbf8efa65d3d877c32d820401ee529
Andrew Pyle <andrew.pyle@icloud.com>
2022-01-21 15:05:29 -0600

    Fix broken css link in build

--- src/lib/Search.svelte
+++ src/lib/Search.svelte
@@ -17,35 +17,4 @@
     display: block;
     width: 100%;
   }
-  /* Give the elements matching internal straight sides, 
-         making one large pill. Stacks into multipls pills
-         on small screens */
-  .multi-element-pill {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    align-items: center;
-  }
-  .multi-element-pill :first-child {
-    border-top-left-radius: 100px;
-    border-bottom-left-radius: 100px;
-  }
-  .multi-element-pill :last-child {
-    border-top-right-radius: 100px;
-    border-bottom-right-radius: 100px;
-  }
-  @media screen and (max-width: 750px) {
-    /* Remove the matching straight sides, making both pills */
-    .multi-element-pill > * {
-      border-radius: 100px;
-    }
-  }
-  @media screen and (max-width: 750px) {
-    /* When its on own line,
-           make input as wide as container for easier typing */
-    .multi-element-pill :first-child {
-      width: 100%;
-      margin-bottom: 0.5em;
-    }
-  }
 </style>
